6bb86f858bd402b2af4b496c9a4fbb34b525ee77
closortiz <aortiz.carlos@gmail.com>
2016-03-23 12:21:45 -0400

    Update Maze2.cpp

--- Maze2.cpp
+++ Maze2.cpp
@@ -9,8 +9,39 @@ void Maze2::find_exit()
    // Use the BFS method to fill the array of predecessors
   Position nbr, current;
   direction d;
+  
+  M[start.row][start.col] = VISITED;
+  Pred[start.row][start.col] = VISITED
+  Q.push(start);
+  
+  while(!Q.empty())
+  {
+	  current = Q.back();
+	  
+	  for(d = DOWN, nbr = current.Neighbor(d); d != NONE; d = next_direction(d), nbr = current.Neighbor(d))
+	  {
+       cout << "executing for" << endl;
+       cout << nbr << endl;
+       
+		  if(M[nbr.row][nbr.col] == OPEN)
+		  {
+        cout << "if " << endl;
+			   M[nbr.row][nbr.col] = VISITED;
+         Q.push(nbr);
+         pred[current.row][current.col] = M[current.row][current.col];                       
+         if(nbr == exitpos)
+         { 
+           return;
+         }
+         
+		  }         
+	  }
+	  
+  }
 
+  
 }
+
 
 
 
